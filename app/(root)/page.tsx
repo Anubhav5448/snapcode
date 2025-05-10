55063cccf27c56653f307e686554921730bfbeed
Anubhav5448 <Anubhav1374@gmail.com>
2025-05-10 22:10:44 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Page() {
+  return (
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Libray" />
+      <h1 className="text-2xl font-karla ">Hello baby </h1>
+    </main>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const user = {};
+  return (
+    <header className="navbar">
+      <nav>
+        <Link href={"/"}>
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+
+        {user && (
+          <figure>
+            <button>
+              <Image
+                src="/assets/images/dummy.jpg"
+                alt="user"
+                width={36}
+                height={36}
+                className="rounded-full aspect-square"
+              />
+            </button>
+            <button className="cursor-pointer">
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+                className="rotate-180"
+              />
+            </button>
+          </figure>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
